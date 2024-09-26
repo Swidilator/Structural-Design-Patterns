1fe5baaaaf420cb42f2ae5d6e0b6d6e885593610
Kyle Weiher <mail@kyleweiher.com>
2024-09-26 15:30:51 +0200

    Changes for lecture 75 - Handle-Body - II

---
+++ patterns/06_Bridge/bridge_04_shared_implementation/SharedString.cpp
@@ -0,0 +1,148 @@
+// File copied directly from course material as no written in lecture
+// Edited to conform to project
+//  Trailing return types
+//  Namespace
+//  Imports instead of includes for project files
+module;
+#include <algorithm>
+#include <iostream>
+
+module bridge.shared_implementation.SharedString:def;
+
+import bridge.shared_implementation.String;
+import bridge.shared_implementation.SharedString;
+
+namespace bridge::shared_implementation
+{
+    auto SharedString::RemoveString() -> void
+    {
+        auto found = m_Strings.find(m_pString);
+        if (found != m_Strings.end())
+        {
+            if (--(*found->second) == 0)
+            {
+                delete m_pString;
+                auto pCount = found->second;
+                delete pCount;
+                m_Strings.erase(found);
+            }
+        }
+    }
+
+
+    SharedString::SharedString(const char* pstr)
+    {
+        auto found = std::find_if(begin(m_Strings), end(m_Strings), [pstr](auto p)
+        {
+            return *p.first == pstr;
+        });
+        if (found != m_Strings.end())
+        {
+            ++(*found->second);
+            m_pString = found->first;
+        }
+        else
+        {
+            String* pTemp = new String{pstr};
+            m_Strings[pTemp] = new size_t{1};
+            m_pString = pTemp;
+        }
+    }
+
+    SharedString::SharedString(const SharedString& other)
+    {
+        if (!other.m_pString)
+            return;
+        auto found = m_Strings.find(other.m_pString);
+        if (found != m_Strings.end())
+        {
+            m_pString = other.m_pString;
+            ++(*found->second);
+        }
+    }
+
+    SharedString::SharedString(SharedString&& other) noexcept
+    {
+        m_pString = other.m_pString;
+        other.m_pString = nullptr;
+    }
+
+    auto SharedString::operator=(const SharedString& other) -> SharedString&
+    {
+        if (this != &other)
+        {
+            if (m_pString)
+            {
+                RemoveString();
+            }
+            auto found = m_Strings.find(other.m_pString);
+            if (found != m_Strings.end())
+            {
+                m_pString = other.m_pString;
+                ++(*found->second);
+            }
+        }
+        return *this;
+    }
+
+    auto SharedString::operator=(SharedString&& other) noexcept -> SharedString&
+    {
+        if (this != &other)
+        {
+            if (m_pString)
+            {
+                RemoveString();
+            }
+            m_pString = other.m_pString;
+            other.m_pString = nullptr;
+        }
+        for (auto x : m_Strings)
+        {
+            std::cout << x.first->GetString() << " : " << *(x.second) << std::endl;
+        }
+        return *this;
+    }
+
+    SharedString::~SharedString()
+    {
+        if (m_pString)
+            RemoveString();
+    }
+
+    auto SharedString::GetLength() const -> size_t
+    {
+        if (!m_pString)
+            return 0;
+        return m_pString->GetLength();
+    }
+
+    auto SharedString::GetString() const -> const char*
+    {
+        if (!m_pString)
+            return "";
+        return m_pString->GetString();
+    }
+
+    auto SharedString::Assign(const char* pstr) -> void
+    {
+        if (m_pString)
+        {
+            RemoveString();
+        }
+        auto found = std::find_if(begin(m_Strings), end(m_Strings), [pstr](auto p)
+        {
+            return *p.first == pstr;
+        });
+        if (found != m_Strings.end())
+        {
+            ++(*found->second);
+            m_pString = found->first;
+        }
+        else
+        {
+            String* pTemp = new String{pstr};
+            m_Strings[pTemp] = new size_t{1};
+            m_pString = pTemp;
+        }
+    }
+}
---
+++ patterns/06_Bridge/bridge_04_shared_implementation/SharedString.ixx
@@ -0,0 +1,35 @@
+//
+// Created by Kyle on 2024/09/26.
+//
+module;
+#include <unordered_map>
+
+
+export module bridge.shared_implementation.SharedString;
+
+import bridge.shared_implementation.String;
+
+namespace bridge::shared_implementation
+{
+    export class SharedString
+    {
+        String* m_pString{};
+        auto RemoveString() -> void;
+
+    public:
+        inline static std::unordered_map<String*, size_t*> m_Strings{};
+        SharedString() = default;
+        SharedString(const char* pstr);
+        SharedString(const SharedString& other);
+        SharedString(SharedString&& other) noexcept;
+
+        auto operator=(const SharedString& other) -> SharedString&;
+        auto operator=(SharedString&& other) noexcept -> SharedString&;
+
+        auto GetLength() const -> size_t;
+        auto GetString() const -> const char*;
+        auto Assign(const char* pstr) -> void;
+
+        ~SharedString();
+    };
+}
